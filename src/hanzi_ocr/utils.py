485b8694402b4c1ae22cc8be0d0701fe934a2243
Kauboimausu <malagonngasca@gmail.com>
2026-07-08 17:24:39 -0600

    moved utility to package

--- src/hanzi_ocr/utils.py
+++ src/hanzi_ocr/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import argparse
 
 def find_project_root():
     """
@@ -14,3 +15,16 @@ def find_project_root():
             raise Exception("Directory's root couldn't be found")
         leaf = leaf.parent
     return leaf
+
+# Source - https://stackoverflow.com/a/43357954
+# Posted by Maxim, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-06-16, License - CC BY-SA 4.0
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
