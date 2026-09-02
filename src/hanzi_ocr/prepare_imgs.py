88eefb1e0d43f15ddfe2029687f28356daddffb8
Kauboimausu <malagonngasca@gmail.com>
2026-09-02 16:40:36 -0600

    validation and training splits, along with loading through dataloaders for training

--- src/hanzi_ocr/prepare_imgs.py
+++ src/hanzi_ocr/prepare_imgs.py
@@ -1,57 +1,50 @@
-from PIL import Image
 import os
-from hanzi_ocr import utils
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 import pandas as pd
-from skimage import io
 import torch
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 import cv2
 
-# def transform_images_folder(folder_path):
-# 
-#     img_transform = transforms.Compose([
-#         transforms.Resize(size=(70, 70)),
-#         transforms.CenterCrop(64),
-#         transforms.Grayscale(num_output_channels=1),
-#         transforms.ToTensor()
-#     ])
-# 
-#     return ImageFolder(root=folder_path, transform=img_transform)
-# 
-# def view_random_img(img_ord, type, font_name):
-# 
-#     img_transform = transforms.Compose([
-#         transforms.Resize(size=(70, 70)),
-#         transforms.CenterCrop(64),
-#         transforms.ToTensor()
-#     ])
-# 
-#     root = utils.find_project_root()
-#     path = os.path.join(root, "data/", "hanzi_imgs/", f"{img_ord}_{type}_{font_name}.png")
-# 
-#     with Image.open(path) as img:
-#         if img.mode != "RGB":
-#             img = img.convert("RGB")
-#         img = img_transform(img)
-# 
-#         plot_image(img)
 
 def plot_image(image, char):
     """
     Plots the image along with the character it displays
     """
     plt.title(char)
-    #plt.imshow(image.permute(1, 2, 0))
+    # plt.imshow(image.permute(1, 2, 0))
     plt.imshow(image, cmap="gray", vmin=0, vmax=255)
     plt.axis("off")
     plt.show()
-    
+
+
+def apply_hash(string, hash):
+    """Aplica hash a una cadena
+
+    Párametros
+    ------------
+    string: str
+        Cadena a la que se le hará hash
+    hash: _hashlib.HASH
+        Instancia de hash que se aplicará
+
+    Regresa
+    ------------
+    Valor entero del hash
+    """
+    hash.update(b"{string}")
+
+
 class SynthethicHanziDataset(Dataset):
 
-    def __init__(self, csv_file, img_dir, transform=None):
+    def __init__(
+        self,
+        csv_file,
+        img_dir,
+        split,
+        validation_fonts,
+        normalization_pipeline,
+        augmentation_pipeline=None,
+    ):
         """
         Parameters
         -----------
@@ -59,13 +52,32 @@ class SynthethicHanziDataset(Dataset):
             Complete path file to the csv file which contains the whole manifest for the synthetic hanzi dataset generated with hanzi_image_generation.py
         img_dir: str
             Path to the directory in which the images are stored
-        transform: 
-            The transformation pipeline that is to be applied to the dataset
+        split: ["val", "train"]
+            The split of the data to be returned
+        validation_fonts: list
+            List of the names of the fonts that are to be used for validation, and therefore excluded from training
+        augmentation_pipeline:
+            Stochastic data augmentation pipeline for training
+        normalization_pipeline: 
+            Data standarization pipeline for the images, resizing, normalization, etc
         """
-        
-        self.hanzi_df = pd.read_csv(csv_file)
+
+        hanzi_df = pd.read_csv(csv_file)
+
+        self.split = split
+        if split.lower() == "train":
+            self.hanzi_df = hanzi_df[hanzi_df["font"] not in validation_fonts]
+        elif split.lower() == "valid":
+            self.hanzi_df = hanzi_df[hanzi_df["font"] in validation_fonts]
         self.img_dir = img_dir
-        self.transform = transform
+        self.augmentation_pipeline = augmentation_pipeline
+        self.normalization_pipeline = normalization_pipeline
+        codepoint_map = {}
+        unique_vals = self.hanzi_df["codepoint"].unique()
+        for i, val in enumerate(unique_vals):
+            codepoint_map[val] = i
+        self.codepoint_map = codepoint_map
+        self.validation_fonts = validation_fonts
 
     def __len__(self):
         return len(self.hanzi_df)
@@ -73,17 +85,17 @@ class SynthethicHanziDataset(Dataset):
     def __getitem__(self, index):
         if torch.is_tensor(index):
             index = index.tolist()
-            
+
         img_name = os.path.join(self.img_dir, self.hanzi_df["file name"].iloc[index])
-        
-        #image = io.imread(img_name) skimage 
-        image = cv2.imread(img_name, cv2.COLOR_BGR2GRAY) # cv2
-        char_codepoint = self.hanzi_df["codepoint"].iloc[index] 
-        
-        if self.transform is not None:
-            # image = self.transform(image) torchvision transform
-            image = self.transform(image=image)["image"] # albumentations
-        return {"image": image, "char": char_codepoint}
 
+        # image = io.imread(img_name) skimage
+        image = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)  # cv2
+        char_codepoint = self.hanzi_df["codepoint"].iloc[index]
 
+        if self.split == "train" and self.augmentation_pipeline is not None:
+            # image = self.augmentation_pipeline(image) torchvision transform
+            image = self.augmentation_pipeline(image=image)["image"]  # albumentations
 
+        image = self.normalization_pipeline(image=image)["image"]
+
+        return image, self.codepoint_map[char_codepoint]
--- src/hanzi_ocr/train_hanzi_model.py
+++ src/hanzi_ocr/train_hanzi_model.py
@@ -1,29 +1,19 @@
 import os
 import argparse
-import pandas as pd 
+import pandas as pd
 import torch.optim as optims
 from hanzi_ocr.HMNIST import HMNISTModel
-from hanzi_ocr.train_nn import train_with_early_stopping, evaluate
+from hanzi_ocr.train_nn import train_with_early_stopping
 import torchmetrics
 from torch.nn import CrossEntropyLoss
 import torch
 from src.hanzi_ocr import utils
-from torchvision import transforms
-import albumentations as A 
+import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from src.hanzi_ocr.prepare_imgs import SynthethicHanziDataset
+from src.hanzi_ocr import utils
+from torch.utils.data import DataLoader
 
-def make_transform(rng_seed):
-    pytorch_train_transform = A.Compose(
-    [
-        A.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0)),
-        A.HorizontalFlip(p=0.5),
-        A.ColorJitter(p=0.3),
-        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-        ToTensorV2(),
-    ],
-    seed=rng_seed,
-    )
-    return pytorch_train_transform
 
 def get_device():
     if torch.cuda.is_available():
@@ -32,126 +22,221 @@ def get_device():
         return "mps"
     else:
         return "cpu"
-    
+
+
 def obtain_number_of_classes(opts):
     """Loads the csv and counts the number of classes the model has to have"""
     root = utils.find_project_root()
-    hanzi_df = pd.read_csv(os.path.join(root, opts.data_folder, opts.manifest_folder, opts.manifest_name))
+    hanzi_df = pd.read_csv(
+        os.path.join(root, opts.data_folder, opts.manifest_folder, opts.manifest_name)
+    )
     unique_codepoints = hanzi_df["codepoint"]
     return len(set(list(unique_codepoints)))
+
 
 def train_model(opts):
     """Trains the model according to the hyperparameters given"""
     device = get_device()
     num_classes = obtain_number_of_classes(opts)
-    
+
     model = HMNISTModel(num_classes).to(device)
-    
+
     match optimizer.lower():
         case "adam":
-            optimizer = optims.Adam(params=model.parameters(), lr=opts.lr, betas=(0.9, 0.999))
+            optimizer = optims.Adam(
+                params=model.parameters(), lr=opts.lr, betas=(0.9, 0.999)
+            )
         case "adamw":
             optimizer = optims.AdamW(params=model.parameters())
         case "nag":
-            optimizer = optims.SGD(params=model.parameters(), nesterov=True, momentum=0.9, lr=opts.lr)
+            optimizer = optims.SGD(
+                params=model.parameters(), nesterov=True, momentum=0.9, lr=opts.lr
+            )
         case "momentum":
-            optimizer = optims.SGD(params=model.parameters(), nesterov=False, momentum=0.9, lr=opts.lr)
-    
+            optimizer = optims.SGD(
+                params=model.parameters(), nesterov=False, momentum=0.9, lr=opts.lr
+            )
+
     match metric.lower():
         case "accuracy":
-            metric = torchmetrics.Accuracy(task="multiclass", num_classes=num_classes).to(device)
+            metric = torchmetrics.Accuracy(
+                task="multiclass", num_classes=num_classes
+            ).to(device)
         case "f1":
-            metric = torchmetrics.F1Score(task="multiclass", num_classes=num_classes).to(device)
+            metric = torchmetrics.F1Score(
+                task="multiclass", num_classes=num_classes
+            ).to(device)
         case "f1_macro":
-            metric = torchmetrics.F1Score(task="multiclass", num_classes=num_classes, average="micro").to(device)
-    
+            metric = torchmetrics.F1Score(
+                task="multiclass", num_classes=num_classes, average="micro"
+            ).to(device)
+
     criterion = CrossEntropyLoss()
-    train_with_early_stopping(device, model, 1, 1, criterion, metric, optimizer, None, opts.epochs, opts.patience)
+
+    root = utils.find_project_root()
+
+    csv_path = os.path.join(
+        root, opts.data_folder, opts.manifest_folder, opts.manifest_name
+    )
+    imgs_folder_path = os.path.join(root, opts.data_folder, opts.imgs_folder)
+
+    augmentation_pipeline = A.Compose(
+        [
+            A.Illumination(p=0.4),
+            A.RandomShadow(p=0.7),
+            A.HueSaturationValue(p=0.6),
+            A.AdditiveNoise(p=0.3),
+            A.InvertImg(p=0.15),
+            A.MotionBlur(p=0.25),
+            A.Defocus(p=0.15),
+            A.Perspective(fit_output=True, keep_size=False, p=0.5),
+            A.SafeRotate((-180, 180), p=0.3),
+        ],
+        seed=opts.random_seed,
+    )
+
+    normalization_pipeline = A.Compose(
+        [
+            A.Resize(64, 64),
+            ToTensorV2(),
+        ]
+    )
+
+    train_ds = SynthethicHanziDataset(
+        csv_path,
+        imgs_folder_path,
+        "train",
+        opts.holdout_fonts,
+        normalization_pipeline=normalization_pipeline,
+        augmentation_pipeline=augmentation_pipeline,
+    )
     
+    valid_ds = SynthethicHanziDataset(
+        csv_path,
+        imgs_folder_path,
+        "valid",
+        opts.holdout_fonts,
+        normalization_pipeline=normalization_pipeline,
+        augmentation_pipeline=None,
+    )
     
+    train_loader = DataLoader(train_ds, opts.batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_ds, opts.batch_size, shuffle=False)
+
+    rng = torch.Generator().manual_seed(opts.random_seed)
+
+    train_with_early_stopping(
+        device,
+        model,
+        train_loader,
+        valid_loader,
+        criterion,
+        metric,
+        optimizer,
+        None,
+        opts.epochs,
+        opts.patience,
+    )
+
+
 def main():
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument(
         "--optimizer",
         type=str,
         default="AdamW",
-        help="The name of the optimizer  that will be used during training"
+        help="The name of the optimizer  that will be used during training",
     )
-    
+
     parser.add_argument(
         "--metric",
         type=str,
         default="Accuracy",
-        help="The name of the metric that will be used for validation and early stopping"
+        help="The name of the metric that will be used for validation and early stopping",
     )
-    
+
     parser.add_argument(
         "--lr",
         type=float,
         default=0.05,
-        help="The learning rate the optimizer will use for training"
+        help="The learning rate the optimizer will use for training",
     )
-    
+
     parser.add_argument(
         "--epochs",
         type=int,
         default=100,
-        help="How many epochs the model will be trained for"
+        help="How many epochs the model will be trained for",
     )
-    
+
     parser.add_argument(
         "patience_epochs",
         type=int,
         default=10,
-        help="How many patience epochs training will have for early stopping"
+        help="How many patience epochs training will have for early stopping",
     )
-    
+
     parser.add_argument(
         "batch_size",
         type=int,
         default=128,
-        help="Batch size that will be used for training"
+        help="Batch size that will be used for training",
     )
-    
+
     parser.add_argument(
         "--data_folder",
-        type=str, 
+        type=str,
         default="data/",
-        help="Folder in which the data is stored"
+        help="Folder in which the data is stored",
     )
-    
+
     parser.add_argument(
         "--model_weights_folder",
         type=str,
         default="hanzi_model_weights/",
-        help="Folder in which the model's weight will be stored"
+        help="Folder in which the model's weight will be stored",
     )
-    
+
     parser.add_argument(
         "imgs_folder",
         type=str,
         default="hanzi_imgs",
-        help="Folder in which the synthtic hanzi images are stored"
+        help="Folder in which the synthtic hanzi images are stored",
     )
-    
+
     parser.add_argument(
         "manifest_folder",
         type=str,
         default="hanzi_images_manifest",
-        help="Folder in which the manifest csv for the images is stored"
+        help="Folder in which the manifest csv for the images is stored",
     )
-    
+
     parser.add_argument(
         "manifest_name",
         type=str,
         default="manifest.csv",
-        help="Name of the manifest file"
+        help="Name of the manifest file",
     )
     
+    parser.add_argument(
+        "holdout_fonts",
+        type=str,
+        nargs='+',
+        default=None,
+        help="The fonts that are to be used for validation during training"
+    )
+
+    parser.add_argument(
+        "--random_seed",
+        type=int,
+        default=21,
+        help="Random seed used for transformations and training batch shuffling",
+    )
+
     opts = parser.parse_args()
     train_model(opts)
-    
-    
+
+
 if __name__ == "__main__":
     main()
